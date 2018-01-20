7e279d41741cd3459b5d7ac8e4aab7cf15ce33a4
hayashi <sshayashi0208@gmail.com>
2018-01-20 17:21:12 +0900

    Make it happen error

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Foo,Bar
 # Register your models here.
+admin.site.register(Foo)
+admin.site.register(Bar)
--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class Bar(models.Model):
+    pass
+
+
+class Foo(models.Model):
+    bar = models.ManyToManyField('Foo', blank=True)
